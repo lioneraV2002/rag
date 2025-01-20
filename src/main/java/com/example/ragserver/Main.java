2466e651744f8758d74a98b95057e0a4837332c5
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-20 21:56:58 +0330

    modified Main class

--- src/main/java/com/example/ragserver/Main.java
+++ src/main/java/com/example/ragserver/Main.java
@@ -2,7 +2,7 @@ package com.example.ragserver;
 
 import com.example.ragserver.controller.ServerController;
 
-public class MainServer {
+public class Main {
     public static void main(String[] args) {
         ServerController serverController = new ServerController();
         serverController.startServer();
