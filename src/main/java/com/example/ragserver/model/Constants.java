85e514698c36f7f25f06bbb127303f7277ca082b
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-20 22:03:00 +0330

    written the first implementation of the Constants class, maybe convert it to enum in the future

--- src/main/java/com/example/ragserver/model/Constants.java
+++ src/main/java/com/example/ragserver/model/Constants.java
@@ -1,4 +1,8 @@
 package com.example.ragserver.model;
 
 public class Constants {
+    public static final int SERVER_PORT = 8080;
+    public static final String PGVECTOR_DB_URL = "jdbc:postgresql://pgvector:5432/ragdb";
+    public static final String OLLAMA_API_URL = "http://ollama:8000";
+    public static final String PDF_DIRECTORY = "/data/pdfs";
 }
