c8e0b29ddac46ae577d7888554d7f95f89a254de
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-21 00:37:51 +0330

    added some Constants

--- src/main/java/com/example/ragserver/model/Constants.java
+++ src/main/java/com/example/ragserver/model/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     public static final String PGVECTOR_DB_URL = "jdbc:postgresql://pgvector:5432/ragdb";
     public static final String OLLAMA_API_URL = "http://ollama:8000";
     public static final String PDF_DIRECTORY = "/data/pdfs";
+    public static final String PG_VECTOR_DB_USER = "raguser";
+    public static final String PG_DB_PASSWORD = "ragpassword";
+    public static final int THREADSPOOL_NUM = 10;
 }
