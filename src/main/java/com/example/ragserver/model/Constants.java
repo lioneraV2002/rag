8ea44378f0357e1d798da8e503ebe7a5958f77a4
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-25 20:48:42 +0330

    made some changes, removing pgvector from every single instance of pgvector's existence

--- src/main/java/com/example/ragserver/model/Constants.java
+++ src/main/java/com/example/ragserver/model/Constants.java
@@ -2,10 +2,8 @@ package com.example.ragserver.model;
 
 public class Constants {
     public static final int SERVER_PORT = 8080;
-    public static final String PGVECTOR_DB_URL = "jdbc:postgresql://pgvector:5432/ragdb";
-    public static final String OLLAMA_API_URL = "http://ollama:8000";
-    public static final String PDF_DIRECTORY = "/data/pdfs";
-    public static final String PG_VECTOR_DB_USER = "raguser";
-    public static final String PG_DB_PASSWORD = "ragpassword";
+    public static final String OLLAMA_API_URL = "http://ollama:11434/api/generate";
+    public static final String TXT_DIRECTORY = "/com/example/ragserver/pdfs";
+
     public static final int THREADSPOOL_NUM = 10;
 }
