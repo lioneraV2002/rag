6b24968ec3f860191991f340358a1424cd4a7e96
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-25 21:44:10 +0330

    added qdrant vector database container to compose.yml

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ RUN apt-get update && apt-get install -y curl
 # Expose the ports
 EXPOSE 11434
 EXPOSE 8080
+EXPOSE 6333
 
 # Set the entry point to start the Java application and load the model
 RUN curl http://ollama:11434/api/pull -d '{\"model\": \"llama3.2:1b\"}' &
--- src/main/java/com/example/ragserver/model/Constants.java
+++ src/main/java/com/example/ragserver/model/Constants.java
@@ -3,7 +3,7 @@ package com.example.ragserver.model;
 public class Constants {
     public static final int SERVER_PORT = 8080;
     public static final String OLLAMA_API_URL = "http://ollama:11434/api/generate";
-    public static final String TXT_DIRECTORY = "/com/example/ragserver/pdfs";
+    public static final String TXT_DIRECTORY = "/pdfs";
 
     public static final int THREADSPOOL_NUM = 10;
 }
