c901d027e4da0a9210f81d985fe01b79344d0655
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-26 23:43:12 +0330

    made some changes to the env variables of qdrantdb, vector size, and collection name

--- src/main/java/com/example/ragserver/model/Constants.java
+++ src/main/java/com/example/ragserver/model/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static final int SERVER_PORT = 8080;
     public static final String OLLAMA_API_URL = "http://ollama:11434/api/generate";
     public static final String TXT_DIRECTORY = "/pdfs";
-
+    public static final String vectorCollectionName = "vectorDB";
+    public static final int vectorSize = 512;
     public static final int THREADSPOOL_NUM = 10;
 }
