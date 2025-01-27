fdd14ef9f50c046b0a9acd06a9210b16b51885a2
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-27 23:49:32 +0330

    recreated the structure of the whole project, starting anew

---
+++ src/main/java/com/example/ragserver/RagServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ragserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ragserver/RagServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ragserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
