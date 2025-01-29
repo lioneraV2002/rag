1c18f54686317b7ee607451ed63024aec6683916
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-29 09:46:48 +0330

    constants are created

--- src/main/java/com/example/ragserver/model/RagModelsConstants.java
+++ src/main/java/com/example/ragserver/model/RagModelsConstants.java
@@ -5,7 +5,7 @@ public enum RagModelsConstants {
             You are an assistant providing financial and trading support.
             Use the information from the DOCUMENTS section to provide accurate financial suggestions,
             summaries, and analyses for the question in the QUESTION section.
-            If unsure, simply state that you don't know.
+            If there is no relevant information provided in the DOCUMENTS, simply state that you don't know.
           """),
     RAG_SIMILAR_ANSWERS_COUNT("20");
 
