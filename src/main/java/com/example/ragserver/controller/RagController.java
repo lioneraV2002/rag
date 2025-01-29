0dc2d4d5c6841bba705cc66a4d1f55e5471d433f
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-01-29 09:45:30 +0330

    controller of rag stablished

--- src/main/java/com/example/ragserver/controller/RagController.java
+++ src/main/java/com/example/ragserver/controller/RagController.java
@@ -1,49 +1,38 @@
 package com.example.ragserver.controller;
 
 import com.example.ragserver.model.FinancialRag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * REST controller for handling requests related to the RAG service.
  * This controller provides endpoints for uploading PDF documents and querying them.
  */
+
 @RestController
 @RequestMapping("rag")
 public class RagController {
 
+    private static final Logger logger = LoggerFactory.getLogger(RagController.class);
     private final FinancialRag financialRag;
 
     /**
      * Constructs a RagController with the specified FinancialRag service.
-     *
-     * @param financialRag the FinancialRag service for processing PDF uploads and queries.
+     * @param vectorStore the postgres vector store for document storage and retrieval.
+     * @param builder     the chat client builder for generating responses.
      */
-    @Autowired
-    public RagController(FinancialRag financialRag) {
-        this.financialRag = financialRag;
+    public RagController(ChatClient.Builder builder, PgVectorStore vectorStore) {
+        logger.info("Initializing RagController");
+        this.financialRag = new FinancialRag(builder, vectorStore);
+        logger.info("Initialized RagController");
+
     }
 
-    /**
-     * Endpoint for uploading a PDF document.
-     *
-     * @param path the file path of the PDF document to be uploaded.
-     * @return a response entity indicating the result of the upload operation.
-     */
-    @PostMapping("/uploadPDF")
-    public ResponseEntity uploadPDF(@RequestBody String path) {
-        try {
-            financialRag.uploadPDF(path);
-            return ResponseEntity.ok().body("Done!");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return ResponseEntity.internalServerError().build();
-        }
-    }
 
     /**
      * Endpoint for querying the uploaded documents.
@@ -52,9 +41,12 @@ public class RagController {
      * @return a response entity containing the generated response based on the query.
      */
     @PostMapping("/query")
-    public ResponseEntity query(@RequestBody String question) {
+    public ResponseEntity<String> query(@RequestParam(value = "prompt") String question) {
         try {
+            logger.info("RagController query question is: {}", question);
             String response = financialRag.query(question);
+            logger.info("RagController query response is: {}", response);
+            logger.info("Returning response <{}> to user", response);
             return ResponseEntity.ok().body(response);
         } catch (Exception e) {
             System.out.println(e.getMessage());
